36ed17639f3a2f1876cf7f78887f56d2e02c7a35
codeWriter9 <nerdsubhankar2006@gmail.com>
2022-03-28 16:05:02 +0530

    Add wikipedia.py
    Fetch the Main Wikipedia page and extract links

---
+++ wikipedia.py
@@ -0,0 +1,30 @@
+#!./bin/python3
+import pandas
+import requests
+from bs4 import BeautifulSoup
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+response = requests.get("https://en.wikipedia.org/wiki/Main_Page",headers=headers)
+
+
+content = response.content
+soup = BeautifulSoup(content,"html.parser")
+
+#print(soup)
+
+link_set = set()
+links = soup.find_all("a")
+for link in links:
+    href = link.get('href')
+    if href and not href.startswith('/wiki'):
+        link_set.add(href)
+    elif href and href.endswith('/wiki/'):
+        continue
+    elif href:
+        link_set.add('https://en.wikipedia.org' + href)
+    #continue
+
+for link_s in link_set:
+    if not link_s.endswith('/wiki/') and link_s.startswith('https'):
+        print(link_s)
